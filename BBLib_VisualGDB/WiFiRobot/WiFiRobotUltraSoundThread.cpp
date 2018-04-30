32945fdf8286ec8c1804f1bfac2ce1dc36aa8f44
forwardkth <forward.li.kth@gmail.com>
2018-04-30 21:13:04 +0200

    Add Class WiFiRobotTCPReceiverThread for the formal WiFiRobot project.

--- BBLib_VisualGDB/WiFiRobot/WiFiRobotUltraSoundThread.cpp
+++ BBLib_VisualGDB/WiFiRobot/WiFiRobotUltraSoundThread.cpp
@@ -1,19 +1,20 @@
-/* Class WiFiRobotUltraSoundThread (UltraSonic sensor for distance meaurement and Object detection)
-*  This is my Remote operation Robot project based on Beaglebone and Debian Linux.
-*  Created on: April 29, 2018
-*  Author: Chao Li
-*  Email:forward.li.kth@gmail.com
-*  Github: https://github.com/forwardkth/BBRobot_VisualGDB
-*  Github: https://github.com/forwardkth/Blacklib_template
-*  Blog: http://forwardkth.github.io/
-*  Project introduction:  http://forwardkth.github.io/2015/08/28/wifi-robot-gen-two/
-*/
+// WiFiRobotUltraSoundThread.cpp
+// Class WiFiRobotUltraSoundThread (UltraSonic sensor for distance meaurement and Object detection)
+//  This is my Remote operation Robot project based on Beaglebone and Debian Linux.
+//  Created on: April 29, 2018
+//  Author: Chao Li
+//  Email:forward.li.kth@gmail.com
+//  Github: https://github.com/forwardkth/BBRobot_VisualGDB
+//  Github: https://github.com/forwardkth/Blacklib_template
+//  Blog: http://forwardkth.github.io/
+//  Project introduction:  http://forwardkth.github.io/2015/08/28/wifi-robot-gen-two/
 
 #include "WiFiRobotUltraSoundThread.h"
 
 int ultra_distance = 100;
 int lowlen = 0;
 int highlen = 0;
+
 namespace WiFiRobot {
 
 UltraSound::UltraSound(BlackUART &serial,
