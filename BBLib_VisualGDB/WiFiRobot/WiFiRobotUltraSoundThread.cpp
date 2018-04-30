19522515d51cd8596b6ac606c5069fdb6b2c2c2e
forwardkth <forward.li.kth@gmail.com>
2018-04-30 21:11:20 +0200

    Add Class WiFiRobotUltraSoundThread for the formal WiFiRobot project.

--- BBLib_VisualGDB/WiFiRobot/WiFiRobotUltraSoundThread.cpp
+++ BBLib_VisualGDB/WiFiRobot/WiFiRobotUltraSoundThread.cpp
@@ -8,3 +8,54 @@
 *  Blog: http://forwardkth.github.io/
 *  Project introduction:  http://forwardkth.github.io/2015/08/28/wifi-robot-gen-two/
 */
+
+#include "WiFiRobotUltraSoundThread.h"
+
+int ultra_distance = 100;
+int lowlen = 0;
+int highlen = 0;
+namespace WiFiRobot {
+
+UltraSound::UltraSound(BlackUART &serial,
+                       int &distance,
+                       int &low,
+                       int &high)
+    : uart(serial),
+      range(distance),
+      Lowlen(low),
+      Highlen(high) { }
+
+UltraSound::~UltraSound() { }
+
+void UltraSound::onStartHandler() {
+  uart.setReadBufferSize(16);
+  if (uart.open(BlackLib::ReadWrite)) {
+    std::cout << std::endl;
+    std::cout << "Device Path     : " << uart.getPortName() << std::endl;
+    std::cout << "Read Buf. Size  : " << uart.getReadBufferSize() << std::endl;
+    std::cout << "BaudRate In/Out : " << uart.getBaudRate(BlackLib::input) << "/"
+              << uart.getBaudRate(BlackLib::output) << std::endl;
+    std::cout << "Character Size  : " << uart.getCharacterSize() << std::endl;
+    std::cout << "Stop Bit Size   : " << uart.getStopBits() << std::endl;
+    std::cout << "Parity          : " << uart.getParity() << std::endl << std::endl;
+
+    char writeArr[4] = "U\r\n";
+    char readArr[22];
+    memset(readArr, 0, sizeof(readArr));
+    //string readt;
+    while (1) {
+      uart.flush(BlackLib::direction::bothDirection);
+      uart.write(writeArr, sizeof(writeArr));
+      msleep(500);
+      uart.read(readArr, sizeof(readArr));
+      Highlen = (int)readArr[0];
+      Lowlen = (int)readArr[1];
+      range = Highlen * 256 + Lowlen;
+      std::cout<< "distance forward: "<<range<<std::endl;
+    }
+    return;
+  }
+
+}
+
+}
--- BBLib_VisualGDB/WiFiRobot/WiFiRobotUltraSoundThread.h
+++ BBLib_VisualGDB/WiFiRobot/WiFiRobotUltraSoundThread.h
@@ -1,15 +1,41 @@
-/* Class WiFiRobotUltraSoundThread (UltraSonic sensor for distance meaurement and Object detection)
-*  This is my Remote operation Robot project based on Beaglebone and Debian Linux.
-*  Created on: April 29, 2018
-*  Author: Chao Li
-*  Email:forward.li.kth@gmail.com
-*  Github: https://github.com/forwardkth/BBRobot_VisualGDB
-*  Github: https://github.com/forwardkth/Blacklib_template
-*  Blog: http://forwardkth.github.io/
-*  Project introduction:  http://forwardkth.github.io/2015/08/28/wifi-robot-gen-two/
-*/
+// WiFiRobotUltraSoundThread.h
+// Class WiFiRobotUltraSoundThread (UltraSonic sensor for distance meaurement and Object detection)
+// This is my Remote operation Robot project based on Beaglebone and Debian Linux.
+// Created on: April 29, 2018
+// Author: Chao Li
+// Email:forward.li.kth@gmail.com
+// Github: https://github.com/forwardkth/BBRobot_VisualGDB
+// Github: https://github.com/forwardkth/Blacklib_template
+// Blog: http://forwardkth.github.io/
+// Project introduction:  http://forwardkth.github.io/2015/08/28/wifi-robot-gen-two/
+
 #ifndef WIFIROBOT_WIFIROBOT_ULTRASOUND_THREAD_H_
 #define WIFIROBOT_WIFIROBOT_ULTRASOUND_THREAD_H_
 
+#include <iostream>
+#include <string>
+#include "../BlackLib/BlackThread/BlackThread.h"
+#include "../BlackLib/BlackMutex/BlackMutex.h"
+#include "../BlackLib/BlackUART/BlackUART.h"
 
-#endif /* WIFIROBOT_WIFIROBOT_ULTRASOUND_THREAD_H_ */
+using namespace BlackLib;
+
+namespace WiFiRobot {
+
+class UltraSound : public BlackLib::BlackThread {
+ public:
+  UltraSound(BlackUART &serial,
+             int &distance, 
+             int &low, int &high);
+  ~UltraSound();
+  void onStartHandler();
+
+private:
+ BlackUART &uart;
+ int &range;
+ int &Lowlen;
+ int &Highlen;
+};
+
+}//namespace WiFiRobot
+#endif // WIFIROBOT_WIFIROBOT_ULTRASOUND_THREAD_H_
