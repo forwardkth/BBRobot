0e21c84044efb07ee3d692a83c4fd59c94e18a84
forwardkth <forward.li.kth@gmail.com>
2018-04-30 21:57:26 +0200

    Add Class WiFiRobotTCPReceiverThread for the formal WiFiRobot project.

--- BBLib_VisualGDB/WiFiRobot/WiFiRobotTCPSenderThread.cpp
+++ BBLib_VisualGDB/WiFiRobot/WiFiRobotTCPSenderThread.cpp
@@ -1,15 +1,52 @@
-/* Class WiFiRobotTCPReceiverThread (TCP socket contection and TX thread)
-*  This is my Remote operation Robot project based on Beaglebone and Debian Linux.
-*  Created on: April 29, 2018
-*  Author: Chao Li
-*  Email:forward.li.kth@gmail.com
-*  Github: https://github.com/forwardkth/BBRobot_VisualGDB
-*  Github: https://github.com/forwardkth/Blacklib_template
-*  Blog: http://forwardkth.github.io/
-*  Project introduction:  http://forwardkth.github.io/2015/08/28/wifi-robot-gen-two/
-*/
+// Class WiFiRobotTCPReceiverThread (TCP socket contection and TX thread)
+// This is my Remote operation Robot project based on Beaglebone and Debian Linux.
+// Created on: April 29, 2018
+// Author: Chao Li
+// Email:forward.li.kth@gmail.com
+// Github: https://github.com/forwardkth/BBRobot_VisualGDB
+// Github: https://github.com/forwardkth/Blacklib_template
+// Blog: http://forwardkth.github.io/
+// Project introduction:  http://forwardkth.github.io/2015/08/28/wifi-robot-gen-two/
 
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#include <netdb.h>
-#include <unistd.h>
+#include "WiFiRobotTCPSenderThread.h"
+
+const int TCP_PORT_TX = 2003; //Listen on Port 2003 for TCP TX
+
+namespace WiFiRobot {
+
+void TCPSenderThread:: onStartHandler() { //Runnable
+  struct sockaddr_in serverAddr;
+  struct sockaddr_in clientAddr;
+  int port = TCP_PORT_TX;   // TCP server TX port
+  int serverSocket = socket(AF_INET, SOCK_STREAM, 0); // creat and initialize a socket
+  int val = 1; // optional setting£¬try to avoid the server can not be restart quickly
+  setsockopt(serverSocket, SOL_SOCKET, SO_REUSEADDR, &val, sizeof(val));
+  serverAddr.sin_family = AF_INET; // define the listening port and address
+  serverAddr.sin_port = htons(port);
+  serverAddr.sin_addr.s_addr = htonl(INADDR_ANY);
+  memset(&(serverAddr.sin_zero), 0, 8);
+  int rc = bind(serverSocket, 
+                (struct sockaddr*) &serverAddr,
+                sizeof(struct sockaddr));
+    if (rc == -1) {
+        std::cout << "bind failed!" << std::endl;
+        exit(1);
+    }
+        // start listening and the max client number is 5
+        std::cout << "TCP TX Listening..." << std::endl;
+        rc = listen(serverSocket, 100);
+        if (rc == -1) {
+            std::cout << "listen failed!" << std::endl;
+            exit(1);
+    } else {
+      std::cout << "TCP TX socket connected!"  << std::endl; // waiting for a connection
+    }
+    int sock;
+    int clientAddrSize = sizeof(struct sockaddr_in);
+    // To be continue...
+}
+
+TCPSenderThread::TCPSenderThread() { }
+TCPSenderThread::~TCPSenderThread() { }
+
+} // namespace WiFiRobot
--- BBLib_VisualGDB/WiFiRobot/WiFiRobotTCPSenderThread.h
+++ BBLib_VisualGDB/WiFiRobot/WiFiRobotTCPSenderThread.h
@@ -1,15 +1,42 @@
-/* Class WiFiRobotTCPReceiverThread (TCP socket contection and TX thread)
-*  This is my Remote operation Robot project based on Beaglebone and Debian Linux.
-*  Created on: April 29, 2018
-*  Author: Chao Li
-*  Email:forward.li.kth@gmail.com
-*  Github: https://github.com/forwardkth/BBRobot_VisualGDB
-*  Github: https://github.com/forwardkth/Blacklib_template
-*  Blog: http://forwardkth.github.io/
-*  Project introduction:  http://forwardkth.github.io/2015/08/28/wifi-robot-gen-two/
-*/
+//  WiFiRobotTCPSenderThread
+//  Class WiFiRobotTCPSenderThread (TCP socket contection and TX thread)
+//  This is my Remote operation Robot project based on Beaglebone and Debian Linux.
+//  Created on: April 29, 2018
+//  Author: Chao Li
+//  Email:forward.li.kth@gmail.com
+//  Github: https://github.com/forwardkth/BBRobot_VisualGDB
+//  Github: https://github.com/forwardkth/Blacklib_template
+//  Blog: http://forwardkth.github.io/
+//  Project introduction:  http://forwardkth.github.io/2015/08/28/wifi-robot-gen-two/
+
 #ifndef WIFIROBOT_WIFIROBOT_TCPSENDER_THREAD_H_
 #define WIFIROBOT_WIFIROBOT_TCPSENDER_THREAD_H_
 
+#include <string>
+#include <iostream>
+#include <cstdlib>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <unistd.h>
+#include "../BlackLib/BlackThread/BlackThread.h"
+#include "../BlackLib/BlackMutex/BlackMutex.h"
+#include "../BlackLib/BlackServo/BlackServo.h"
+#include "../BlackLib/BlackGPIO/BlackGPIO.h"
 
+using namespace BlackLib;
+
+namespace WiFiRobot {
+// TCP thread for order receiver and execution
+class TCPSenderThread : public BlackLib::BlackThread {
+ public:
+  void onStartHandler();// To be implemented
+
+  TCPSenderThread();
+  ~TCPSenderThread();
+};
+
+} //namespace WiFiRobot
 #endif /* WIFIROBOT_WIFIROBOT_TCPSENDER_THREAD_H_ */
